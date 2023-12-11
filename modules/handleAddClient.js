50c096ea73d91a8858e8588cb005a04c34ad07f4
Alexei Web <alexeiiweb@gmail.com>
2023-12-11 11:11:31 +0300

    third

--- modules/handleAddClient.js
+++ modules/handleAddClient.js
@@ -1,56 +1,48 @@
 import { CLIENTS } from "../index.js";
+import { readRequestBody } from "./helpers.js";
 import { sendData, sendError } from "./send.js";
 import fs from "node:fs/promises";
 
-export const handleAddClient = (req, res) => {
-  let body = "";
+export const handleAddClient = async (req, res) => {
   try {
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-  } catch (error) {
-    console.log(`Ошибка при чтении запроса`);
-    sendError(res, 500, "Ошибка сервера при чтении запроса");    
-  }
+    const body = await readRequestBody(req);
+    const newClient = JSON.parse(body);
 
-  req.on("end", async () => {
-    try {
-      const newClient = JSON.parse(body);
-
-      if (
-        !newClient.fullName ||
-        !newClient.phone || 
-        !newClient.ticketNumber
-      ) {
-        sendError(res, 400, "Неверные данные");
-        return;
-      }
-
-      if (!newClient.booking) {
-        sendError(res, 400, "Отсутствует список бронирования");
-        return;
-      }
-
-      if (
-        (newClient.booking &&
-        !newClient.booking.length ||
-        !Array.isArray(newClient.booking) || 
-        !newClient.booking.every((item) => item.comedian && item.time))) 
-        {
-        sendError(res, 400, "Неверно заполнены поля бронирования");
-        return;
-      }
-      
-      const clientData = await fs.readFile(CLIENTS, 'utf-8');
-      const clients = JSON.parse(clientData);
-
-      clients.push(newClient);
-
-      await fs.writeFile(CLIENTS, JSON.stringify(clients));
-
-      sendData(res, newClient);
-    } catch (error) {
-      console.log('error: ', error);      
+    if (
+      !newClient.fullName ||
+      !newClient.phone || 
+      !newClient.ticketNumber ||
+      !newClient.booking
+    ) {
+      sendError(res, 400, "Неверные данные");
+      return;
     }
-  })
-}
+
+    if (!newClient.booking) {
+      sendError(res, 400, "Отсутствует список бронирования");
+      return;
+    }
+
+    if (
+      (newClient.booking &&
+      !newClient.booking.length ||
+      !Array.isArray(newClient.booking) || 
+      !newClient.booking.every((item) => item.comedian && item.time))) 
+      {
+      sendError(res, 400, "Неверно заполнены поля бронирования");
+      return;
+    }
+    
+    const clientData = await fs.readFile(CLIENTS, 'utf-8');
+    const clients = JSON.parse(clientData);
+
+    clients.push(newClient);
+
+    await fs.writeFile(CLIENTS, JSON.stringify(clients));
+
+    sendData(res, newClient);
+  } catch (error) {
+    console.log('error: ', error); 
+    sendError(res, 500, "Ошибка сервера при чтении запроса");     
+  }
+};
--- modules/send.js
+++ modules/send.js
@@ -1,7 +1,7 @@
 export const sendData = (res, data) => {
   res.writeHead(200, {
-    "Content-Type": "text/plain; charset=utf-8",    
-});
+    "Content-Type": "text/json; charset=utf-8",
+  });
   res.end(JSON.stringify(data));
 };
 
